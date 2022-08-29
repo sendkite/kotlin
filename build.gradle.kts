79dcf1c352802521bc55215ed16fc41611e1c527
sendkite <sendkite1@gmail.com>
2022-08-30 00:49:17 +0900

    FEAT: setting test for kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,12 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webflux")
+    // test for kotlin
+    // https://mvnrepository.com/artifact/io.mockk/mockk
+    testImplementation("io.mockk:mockk:1.12.7")
+    // https://github.com/Ninja-Squad/springmockk
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
 }
 
 tasks.withType<KotlinCompile> {
@@ -39,4 +45,12 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+sourceSets {
+    test {
+        java {
+            setSrcDirs(listOf("src/test/intg", "src/test/unit"))
+        }
+    }
 }
